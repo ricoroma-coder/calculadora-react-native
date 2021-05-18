363e2f1aeb71c9a6a68509c1f83a26f92ce955fd
Henrique Roma <roma.fulldev@gmail.com>
2021-05-18 11:13:20 -0300

    Criando o display

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
+import Display from './src/components/Display'
 
 export default function App() {
+
+  const [value, setValue] = useState(0)
+
   return (
     <View style={styles.container}>
+      <Display value={value} />
       <View style={styles.buttons}>
         <Button label='AC' />
         <Button label='/' />
